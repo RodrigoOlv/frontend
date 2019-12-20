a809134b2e11028c77bf8af3f2c514bd2d5c071d
RodrigoOlv <rodrigo.oliveira1998@outlook.com>
2019-12-19 21:47:12 -0300

    initial commit?

---
+++ assets/js/funcoes.js
@@ -0,0 +1,56 @@
+$(function(){
+
+
+});
+
+function selectProduct1() {
+    document.getElementById('comprar1').style.display="block";
+    document.getElementById('sale1').style.borderStyle="solid";
+    document.getElementById('sale1').style.borderColor="#E1E1E1";
+    document.getElementById('sale1').style.borderWidth="1px";
+    document.getElementById('sale1').style.borderRadius="5px";
+}
+
+function unselectProduct1() {
+    document.getElementById('comprar1').style.display="none";
+    document.getElementById('sale1').style.border="none";
+}
+
+function selectProduct2() {
+    document.getElementById('comprar2').style.display="block";
+    document.getElementById('sale2').style.borderStyle="solid";
+    document.getElementById('sale2').style.borderColor="#E1E1E1";
+    document.getElementById('sale2').style.borderWidth="1px";
+    document.getElementById('sale2').style.borderRadius="5px";
+}
+
+function unselectProduct2() {
+    document.getElementById('comprar2').style.display="none";
+    document.getElementById('sale2').style.border="none";
+}
+
+function selectProduct3() {
+    document.getElementById('comprar3').style.display="block";
+    document.getElementById('sale3').style.borderStyle="solid";
+    document.getElementById('sale3').style.borderColor="#E1E1E1";
+    document.getElementById('sale3').style.borderWidth="1px";
+    document.getElementById('sale3').style.borderRadius="5px";
+}
+
+function unselectProduct3() {
+    document.getElementById('comprar3').style.display="none";
+    document.getElementById('sale3').style.border="none";
+}
+
+function selectProduct4() {
+    document.getElementById('comprar4').style.display="block";
+    document.getElementById('sale4').style.borderStyle="solid";
+    document.getElementById('sale4').style.borderColor="#E1E1E1";
+    document.getElementById('sale4').style.borderWidth="1px";
+    document.getElementById('sale4').style.borderRadius="5px";
+}
+
+function unselectProduct4() {
+    document.getElementById('comprar4').style.display="none";
+    document.getElementById('sale4').style.border="none";
+}
